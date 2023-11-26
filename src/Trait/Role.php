c2a9654dd4090061b72de5166ae9b203857a5b16
remco van der Velde <remco.pc@outlook.com>
2023-11-26 16:43:27 +0100

    init

---
+++ src/Trait/Role.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace R3m\Io\Node\Trait;
+
+use R3m\Io\App;
+use R3m\Io\Config;
+
+Trait Role {
+
+    public function role_system()
+    {
+        $object = $this->object();
+        if(
+            in_array(
+                $object->config(Config::POSIX_ID),
+            [
+                0,
+                33,     //remove this, how to handle www-data events, middleware and filter
+            ]
+            )
+        ){
+            $url = $object->config('project.dir.data') . 'Account' . $object->config('ds') . 'Role.System.json';
+            $data = $object->data_read($url);
+            if($data){
+                return $data->data();
+            }
+            return false;
+        }
+    }
+}
