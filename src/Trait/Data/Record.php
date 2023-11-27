5416e1a02ffdeba4c1e55a4bfbe1be848e8e70fb
remco van der Velde <remco.pc@outlook.com>
2023-11-27 18:42:59 +0100

    init

---
+++ src/Trait/Data/Record.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace R3m\Io\Node\Trait\Data;
+
+use R3m\Io\Node\Service\Security;
+
+use Exception;
+
+use R3m\Io\Exception\FileWriteException;
+use R3m\Io\Exception\ObjectException;
+
+Trait Record {
+
+    /**
+     * @throws ObjectException
+     * @throws FileWriteException
+     * @throws Exception
+     */
+    public function record($class, $role, $options=[]): ?array
+    {
+        $name = Controller::name($class);
+        $options['limit'] = 1;
+        $options['page'] = 1;
+        if(!array_key_exists('function', $options)){
+            $options['function'] = __FUNCTION__;
+        }
+        if(!Security::is_granted(
+            $name,
+            $role,
+            $options
+        )){
+            return null;
+        }
+        $list = $this->list($name, $role, $options);
+        if(
+            is_array($list) &&
+            array_key_exists('list', $list) &&
+            array_key_exists(0, $list['list'])
+        ){
+            $record = $list;
+            $record['node'] = $list['list'][0];
+            unset($record['list']);
+            unset($record['page']);
+            unset($record['limit']);
+            unset($record['count']);
+            return $record;
+        }
+        return null;
+    }
+
+}
