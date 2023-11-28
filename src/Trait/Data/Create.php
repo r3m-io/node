a7a5bea8c9e4fd4cec1d1ffb6f1af37040a6ed8c
remco van der Velde <remco.pc@outlook.com>
2023-11-28 19:35:20 +0100

    count

---
+++ src/Trait/Data/Create.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace R3m\Io\Node\Trait\Data;
+
+use R3m\Io\Module\Controller;
+use R3m\Io\Module\Core;
+
+use Exception;
+
+Trait Create {
+
+    /**
+     * @throws Exception
+     */
+    public function create($class, $role, $node=[], $options=[]): false|array
+    {
+        d($node);
+        ddd($options);
+
+        /*
+        $options = Core::object($options, Core::OBJECT_ARRAY);
+        $count = 0;
+        $name = Controller::name($class);
+        if(!array_key_exists('function', $options)){
+            $options['function'] = 'count';
+        }
+        if(!array_key_exists('limit', $options)){
+            $options['limit'] = '*';
+        }
+        unset($options['page']);
+        $response = $this->list($class, $role, $options);
+        if(
+            !empty($response) &&
+            is_array($response) &&
+            array_key_exists('count', $response)
+        ){
+            $count = $response['count'];
+        }
+        return $count;
+        */
+        return false;
+    }
+}
