269a6724d1410c2ada2e8f0540933142bddd3f48
remco van der Velde <remco.pc@outlook.com>
2023-11-28 17:53:07 +0100

    delete

--- src/Service/Security.php
+++ src/Service/Security.php
@@ -31,13 +31,13 @@ class Security extends Main
             array_key_exists('relation', $options) &&
             $options['relation'] === true
         ){
-            $permissions[] = $name_permission . '.' . $function_permission . '.' . 'relation';
+            $permissions[] = $name_permission . ':' . $function_permission . '.' . 'relation';
         }
         if(
             array_key_exists('parse', $options) &&
             $options['parse'] === true
         ){
-            $permissions[] = $name_permission . '.' . $function_permission . '.' . 'parse';
+            $permissions[] = $name_permission . ':' . $function_permission . '.' . 'parse';
         }
         $role_permissions = $role->get('permission');
         if(is_array($role_permissions)){
@@ -50,7 +50,7 @@ class Security extends Main
                     array_key_exists('relation', $options) &&
                     $options['relation'] === true
                 ){
-                    if($permission->get('name') === $name_permission . '.' .$function_permission . '.' . 'relation'){
+                    if($permission->get('name') === $name_permission . ':' .$function_permission . '.' . 'relation'){
                         $is_permission_relation = true;
                     }
                 } else {
@@ -60,7 +60,7 @@ class Security extends Main
                     array_key_exists('parse', $options) &&
                     $options['parse'] === true
                 ) {
-                    if($permission->get('name') === $name_permission . '.' . $function_permission . '.' . 'parse'){
+                    if($permission->get('name') === $name_permission . ':' . $function_permission . '.' . 'parse'){
                         $is_permission_parse = true;
                     }
                 } else {
