aba2f056f5b07b5c9e410f0e8401dccfec3d7243
remco van der Velde <remco.pc@outlook.com>
2023-11-28 20:49:09 +0100

    count

--- src/Model/Node.php
+++ src/Model/Node.php
@@ -7,9 +7,11 @@ use R3m\Io\Module\Data as Storage;
 use R3m\Io\Module\Template\Main;
 
 use R3m\Io\Node\Trait\Data;
+use R3m\Io\Node\Trait\Role;
 
 class Node extends Main {
     use Data;
+    use Role;
 
     public function __construct(App $object){
         $this->object($object);
