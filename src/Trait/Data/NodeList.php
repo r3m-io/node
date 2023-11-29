9629bdb064b36611dbe4f316857f0652b014bbfb
remco van der Velde <remco.pc@outlook.com>
2023-11-29 23:34:17 +0100

    page

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -125,6 +125,10 @@ trait NodeList {
                     $options['where'] = $this->where_convert($options['where']);
                     $is_where = true;
                 }
+                $debug = debug_backtrace(true);
+                d($debug[0]['file'] . ' ' . $debug[0]['line'] . ' ' . $debug[0]['function']);
+                d($debug[1]['file'] . ' ' . $debug[1]['line'] . ' ' . $debug[1]['function']);
+                d($debug[2]['file'] . ' ' . $debug[2]['line'] . ' ' . $debug[2]['function']);
                 ddd($list);
                 foreach($list as $nr => $record) {
                     if(
