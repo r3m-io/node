890a58af2c751432cebbf2a3a13c84d021146185
remco van der Velde <remco.pc@outlook.com>
2023-11-30 18:27:36 +0100

    page

--- src/Trait/Where.php
+++ src/Trait/Where.php
@@ -412,6 +412,7 @@ trait Where {
                             ]
                         ];
                         $and = Filter::list($list)->where($filter_where);
+                        d($and);
                         if(!empty($and)){
                             $where[$key] = true;
                             $set[0] = true;
