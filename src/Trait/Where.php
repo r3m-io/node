f279182a44ccff35d442308ec8ce04548d478da8
remco van der Velde <remco.pc@outlook.com>
2023-12-01 10:21:52 +0100

    page

--- src/Trait/Where.php
+++ src/Trait/Where.php
@@ -664,9 +664,9 @@ trait Where {
             }
             $set = $this->where_get_set($where, $key, $deepest);
             while($record !== false){
-                d($set);
+                //d($set);
                 $set = $this->where_process($record, $set, $where, $key, $operator, $options);
-                d($set);
+                //d($set);
 //                d($record);
 //                d($deepest);
                 if(empty($set) && $deepest === 0){
