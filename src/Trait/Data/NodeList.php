5fe3692a9fcceb09178d079add9dd35af1016d1b
remco van der Velde <remco.pc@outlook.com>
2023-12-07 18:38:41 +0100

    ramdisk

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -332,6 +332,7 @@ trait NodeList {
                 $result['relation'] = $options['relation'] ?? true;
                 $result['parse'] = $options['parse'] ?? false;
                 $result['mtime'] = $mtime;
+                d($ramdisk_url_node);
                 $result['duration'] = (microtime(true) - $object->config('time.start')) * 1000;
                 if(
                     array_key_exists('ramdisk', $options) &&
