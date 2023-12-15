39155fa0098617849be9f2aeca51b92603fc38b4
remco van der Velde <remco.pc@outlook.com>
2023-12-15 19:19:20 +0100

    trait init

--- src/Package/Trait/Init.php
+++ src/Package/Trait/Init.php
@@ -14,7 +14,8 @@ trait Init {
     /**
      * @throws Exception
      */
-    public function register (){
+    public function register (): void
+    {
         $object = $this->object();
         $options = App::options($object);
         $node = new Node($object);
@@ -27,13 +28,15 @@ trait Init {
         ];
         $class = 'System.Installation';
         $response = $node->record($class, $node->role_system(), $record_options);
-        if($response){
+        if(
+            $response &&
+            array_key_exists('node', $response)
+        ){
             if(property_exists($options, 'force')){
-                d('test2');
-                ddd($response);
+                $record = $response['node'];
+                $record->mtime = time();
+                $response = $node->put($class, $node->role_system(), $record);
             }
-
-            //check for force so we can update mtime
         } else {
             $time = time();
             $record = (object) [
@@ -43,49 +46,5 @@ trait Init {
             ];
             $response = $node->create($class, $node->role_system(), $record);
         }
-
-        /*
-        {{$request = request()}}
-        {{$class = 'System.Installation'}}
-        {{$options = [
-        'where' => 'name === "' + $request.package + '"',
-        ]}}
-        {{$response = R3m.Io.Node:Data:record(
-        $class,
-        R3m.Io.Node:Role:role.system(),
-        $options
-        )}}
-        {{if(is.empty($response))}}
-        {{$output = execute(binary() + ' r3m_io/node create -class=System.Installation -name=' +  $request.package +' -ctime=' + time() + ' -mtime=' + time())}}
-        - {{$request.package}} installed...
-        {{else}}
-        - Skipping {{$request.package}} installation...
-        {{/if}}
-        */
-
-        /*
-        $url_package = $object->config('project.dir.vendor') . 'r3m_io/boot/Data/Package.json';
-        $class = File::basename($url_package, $object->config('extension.json'));
-        $packages = $object->data_read($url_package);
-        $node = new Node($object);
-        if($packages){
-            foreach($packages->data($class) as $nr => $package){
-                $options = [
-                    'where' => 'name === "' . $package  . '"'
-                ];
-                $response = $node->record(
-                    'System.Installation',
-                    $node->role_system(),
-                    $options
-                );
-                if(!$response){
-                    $command = Core::binary($object) . ' install ' . $package;
-                    Core::execute($object, $command);
-                } else {
-                    echo 'Skipping ' . $package . ' installation...' . PHP_EOL;
-                }
-            }
-        }
-        */
     }
 }
