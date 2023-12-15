a10c49ccc2ca9f63427cef63717b7d00089f2383
remco van der Velde <remco.pc@outlook.com>
2023-12-15 23:29:25 +0100

    where bug

--- src/Trait/Data/Create.php
+++ src/Trait/Data/Create.php
@@ -117,13 +117,119 @@ Trait Create {
         return $response;
     }
 
-    public function create_many($class, $role, $nodeList=[], $options=[]): void
+    /**
+     * @throws Exception
+     */
+    public function create_many($class, $role, $nodeList=[], $options=[])
     {
-        d($class);
-        d($role);
-        d($nodeList);
-        d($options);
-        ddd('end');
-
+        $name = Controller::name($class);
+        $object = $this->object();
+        if(!array_key_exists('function', $options)){
+            $options['function'] = __FUNCTION__;
+        }
+        $options['relation'] = false;
+        if(!Security::is_granted(
+            $class,
+            $role,
+            $options
+        )){
+            return false;
+        }
+        $dir_data = $object->config('project.dir.node') .
+            'Data' .
+            $object->config('ds')
+        ;
+        $url = $dir_data .
+            $name .
+            $object->config('extension.json')
+        ;
+        $dir_validate = $object->config('project.dir.node') .
+            'Validate'.
+            $object->config('ds')
+        ;
+        $validate_url =
+            $dir_validate .
+            $name .
+            $object->config('extension.json');
+        $list = [];
+        $result = [];
+        $error = [];
+        foreach($nodeList as $nr => $node){
+            if(
+                is_object($node) &&
+                get_class($node) === Storage::class
+            ){
+                $node = $node->data();
+            } else {
+                $node = Core::object($node, Core::OBJECT_OBJECT);
+            }
+            $object->request('node', $node);
+            $object->request('node.uuid', Core::uuid());
+            $object->request('node.#class', $name);
+            if(
+                array_key_exists('validation', $options) &&
+                $options['validation'] === false
+            ){
+                $validate = (object) ['success' => true];
+            } else {
+                $validate = $this->validate($object, $validate_url,  $name . '.create');
+            }
+            if($validate) {
+                if ($validate->success === true) {
+                    $expose = $this->expose_get(
+                        $object,
+                        $name,
+                        $name . '.' . __FUNCTION__ . '.expose'
+                    );
+                    $node = new Storage();
+                    $node->data($object->request('node'));
+                    $node->set('#class', $name);
+                    if (
+                        $expose &&
+                        $role
+                    ) {
+                        $node = $this->expose(
+                            $node,
+                            $expose,
+                            $name,
+                            __FUNCTION__,
+                            $role
+                        );
+                        $record = $node->data();
+                        if(Core::object_is_empty($record)){
+                            throw new Exception('Empty node after expose...');
+                        }
+                        $list[] = $record;
+                        $result[] = $record->uuid;
+                    }
+                } else {
+                    $error[] = $validate->test;
+                }
+            }
+        }
+        if(empty($list)){
+            return false;
+        }
+        if(!empty($error)){
+            $response = [];
+            $response['error'] = $error;
+            return $response;
+        }
+        $data = $object->data_read($url);
+        if(!$data){
+            $data = new Storage();
+        } else {
+            $list = array_merge($data->get($name), $list);
+        }
+        $data->set($name, $list);
+        $write = $data->write($url);
+        $this->sync_file([
+            'dir_data' => $dir_data,
+            'url' => $url,
+        ]);
+        $response = [];
+        $response['list'] = $result;
+        $response['byte'] = $write;
+        return $response;
     }
 }
