e7f25a58c662f4e59fe3f8212f3bd31a563ed4a6
remco van der Velde <remco.pc@outlook.com>
2024-01-16 16:43:01 +0100

    Cli (phpstan), cleanup

--- src/Validator/Is.Unique.php
+++ src/Validator/Is.Unique.php
@@ -130,7 +130,7 @@ function validate_is_unique(App $object, $value='', $attribute='', $validate='')
             ]
         ];
     }
-    ddd(validate);
+    ddd($validate);
     $node = new Node($object);
     $response = $node->record($name, $node->role_system(), $options);
     if(
