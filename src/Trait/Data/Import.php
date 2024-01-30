12abe78b701f709a2d000da11d23fac71d65a519
remco van der Velde <remco.pc@outlook.com>
2024-01-30 12:35:08 +0100

    added unlock to operation

--- src/Trait/Data/Import.php
+++ src/Trait/Data/Import.php
@@ -86,7 +86,6 @@ trait Import {
                 $list_count = count($list);
                 if($list_count > 1000){
                     $list = array_chunk($list, 1000);
-                    $index = 0;
                     foreach($list as $chunk){
                         $filter_value_1 = [];
                         $filter_value_2 = [];
@@ -95,8 +94,6 @@ trait Import {
                         foreach($chunk as $record){
                             $node = new Storage();
                             $node->data($record);
-                            $node->data('#index', $index);
-                            $index++;
                             if (
                                 $data_object &&
                                 $data_object->has('is.unique')
@@ -109,7 +106,7 @@ trait Import {
                                     $explode[$nr] = trim($value);
                                     $count++;
                                 }
-                                $explode[1] = '#index';
+                                $explode[1] = 'id';
                                 $count = 2;
                                 switch ($count) {
                                     case 2:
