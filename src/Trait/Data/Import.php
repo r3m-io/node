29965c72bcc599a13ee5b8e8b25730044bb8bef5
remco van der Velde <remco.pc@outlook.com>
2024-02-11 09:24:56 +0100

    bugfix node import

--- src/Trait/Data/Import.php
+++ src/Trait/Data/Import.php
@@ -236,7 +236,21 @@ trait Import {
                     }
                     switch($count){
                         case 0 :
-                            $create_many = $chunk;
+                            if (
+                                array_key_exists('force', $options) &&
+                                $options['force'] === true
+                            ) {
+                                $put_many[] = $chunk;
+                            } elseif (
+                                array_key_exists('patch', $options) &&
+                                $options['patch'] === true
+                            ){
+                                $patch_many[] = $chunk;
+                            } else {
+                                $create_many = $chunk;
+                            }
+                            d($put_many);
+                            d($patch_many);
                             ddd($create_many);
                             $response = $this->update(
                                 $class,
