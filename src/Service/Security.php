c928079c1cd3d8b9e4431074c2010c72633df54a
remco van der Velde <remco.pc@outlook.com>
2024-03-11 18:01:44 +0100

    debug permission

--- src/Service/Security.php
+++ src/Service/Security.php
@@ -39,10 +39,7 @@ class Security extends Main
         ){
             $permissions[] = $name_permission . ':' . $function_permission . '.' . 'parse';
         }
-        $role_permissions = $role->get('permissions');
-        if(empty($role_permissions)){
-            $role_permissions = $role->get('permission');
-        }
+        $role_permissions = $role->get('permission');
         if(is_array($role_permissions)){
             foreach($role_permissions as $permission){
                 $permission = new Data($permission);
