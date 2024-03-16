333e737d4c2026db05c182602cb7e40d3799acc2
remco van der Velde <remco.pc@outlook.com>
2024-03-16 23:22:28 +0100

    bugfix transaction unlock & lock

--- src/Validator/Is.Unique.php
+++ src/Validator/Is.Unique.php
@@ -137,7 +137,6 @@ function validate_is_unique(App $object, $value='', $attribute='', $validate='',
     }
     $node = new Node($object);
     $uuid = $object->request('node.uuid');
-    d($uuid);
     //add cache dir to record
     $response = $node->record($name, $node->role_system(), $options);
     if(
@@ -151,6 +150,8 @@ function validate_is_unique(App $object, $value='', $attribute='', $validate='',
             property_exists($record, 'uuid') &&
             !empty($record->uuid)
         ){
+            d($uuid);
+            d($record->uuid);
             if($uuid === $record->uuid){
                 //can patch, can put
                 return true;
