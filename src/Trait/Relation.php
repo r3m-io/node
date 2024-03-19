8861626146875911583c57caa2140fd264965e5f
remco van der Velde <remco.pc@outlook.com>
2024-03-19 20:08:29 +0100

    validator is.relation

--- src/Trait/Relation.php
+++ src/Trait/Relation.php
@@ -126,9 +126,6 @@ trait Relation {
                                 $one_many = $node->get($relation->attribute);
                                 if(is_object($one_many)){
                                     if(!property_exists($one_many, 'limit')){
-                                        d($node);
-                                        d($relation);
-                                        ddd($one_many);
                                         throw new Exception('Relation: ' . $relation->attribute . ' has no limit');
                                     }
                                     if(!property_exists($one_many, 'page')){
@@ -190,6 +187,8 @@ trait Relation {
                                             'uuid' => 'ASC'
                                         ];
                                     }
+                                    d($relation);
+                                    d($one_many);
                                     $response = $this->list(
                                         $relation->class,
                                         $this->role_system(),
