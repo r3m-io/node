baf050dcc9e92bed11621e9f16a2ce340d2f19bb
remco van der Velde <remco.pc@outlook.com>
2024-04-14 22:54:08 +0200

    adding view create

--- src/Trait/View.php
+++ src/Trait/View.php
@@ -37,19 +37,55 @@ Trait View {
         ;
         $data = $object->data_read($source);
         $list = [];
+        $options_name = '';
+        $options_name_finished = false;
         if($data){
             $count = 0;
             foreach($data->data($name) as $record){
                 $count++;
                 $node = new Storage($record);
                 $new = new Storage();
-                foreach($options->attribute as $nr => $attribute){
-                    $new->data($attribute, $node->data($attribute));
+                if(
+                    property_exists($options, 'attribute') &&
+                    is_array($options->attribute)
+                ){
+                    foreach($options->attribute as $nr => $attribute){
+                        if(
+                            $options_name_finished === false &&
+                            $options_name === ''
+                        ){
+                            $options_name = $attribute;
+                        }
+                        elseif($options_name_finished === false) {
+                            $options_name .= '-' . $attribute;
+                        }
+                        $new->data($attribute, $node->data($attribute));
+                    }
+                    $options_name_finished = true;
                 }
+                $new->data('uuid', $node->data('uuid'));
+                $new->data('#class', $node->data('#class'));
                 $list[] = $new->data();
             }
         }
         d(count($list));
+        $list = new Storage($list);
+        if(
+            property_exists($options, 'ramdisk') &&
+            $options->ramdisk === true
+        ){
+            $target = $object->config('ramdisk.url') .
+                $object->config('posix.id') .
+                $object->config('ds') .
+                'Node' .
+                $object->config('ds') .
+                'View' .
+                $object->config('ds') .
+                $name .
+                '-' .
+                $options->name .
+                $object->config('extension.json');
+        }
         ddd($options);
     }
 }
