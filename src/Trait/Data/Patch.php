60d3573b3dd220e648c717a4a45bb131cc8bc7a8
remco van der Velde <remco.pc@outlook.com>
2024-04-28 22:28:43 +0200

    debugging patch

--- src/Trait/Data/Patch.php
+++ src/Trait/Data/Patch.php
@@ -107,6 +107,7 @@ trait Patch {
                     $record = $list[$list_nr];
                 }
             }
+            ddd($object->request());
             d($node);
             $temp = Core::object_merge($record, $node);
             ddd($temp);
