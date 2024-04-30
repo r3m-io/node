f50dea8eebcb910d6b689f8b72861e64623e7ce7
remco van der Velde <remco.pc@outlook.com>
2024-04-30 13:52:25 +0200

    added pre-compile & parse to nodelist

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -741,6 +741,12 @@ trait NodeList {
                     $result['where'] = $options['where'] ?? [];
                     $result['relation'] = $options['relation'] ?? true;
                     $result['parse'] = $options['parse'] ?? false;
+                    if(
+                        array_key_exists('pre-compile', $options) &&
+                        $options['pre-compile'] === true
+                    ){
+                        $result['parse'] = true;
+                    }
                     $result['ramdisk'] = $options['ramdisk'] ?? false;
                     $result['mtime'] = $mtime;
                     $result['transaction'] = $options['transaction'] ?? false;
@@ -799,6 +805,12 @@ trait NodeList {
                 $result['where'] = $options['where'] ?? [];
                 $result['relation'] = $options['relation'] ?? true;
                 $result['parse'] = $options['parse'] ?? false;
+                if(
+                    array_key_exists('pre-compile', $options) &&
+                    $options['pre-compile'] === true
+                ){
+                    $result['parse'] = true;
+                }
                 $result['ramdisk'] = $options['ramdisk'] ?? false;
                 $result['mtime'] = $mtime;
                 $result['transaction'] = $options['transaction'] ?? false;
@@ -851,6 +863,12 @@ trait NodeList {
         }
         $result['relation'] = $options['relation'];
         $result['parse'] = $options['parse'];
+        if(
+            array_key_exists('pre-compile', $options) &&
+            $options['pre-compile'] === true
+        ){
+            $result['parse'] = true;
+        }
         $result['ramdisk'] = $options['ramdisk'] ?? false;
         $result['mtime'] = $mtime;
         $result['transaction'] = $options['transaction'] ?? false;
