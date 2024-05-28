48214500e50a3765f42f59a9159aa68fb56d95ef
remco van der Velde <remco.pc@outlook.com>
2024-05-28 17:10:09 +0200

    debugging for a filter

--- src/Trait/Data/Create.php
+++ src/Trait/Data/Create.php
@@ -112,6 +112,9 @@ trait Create {
             }
             $object->request('node.#class', $name);
             d($object->request());
+
+
+
             if(
                 array_key_exists('validation', $options) &&
                 $options['validation'] === false
