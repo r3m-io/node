694b2d996de421f06c35f9891f4c40c161bed3c1
remco van der Velde <remco.pc@outlook.com>
2024-06-05 23:48:43 +0200

    make expose_get more efficient

--- src/Trait/Where.php
+++ src/Trait/Where.php
@@ -254,6 +254,10 @@ trait Where {
     private function where_get_set(&$where=[], &$key=null, $deep=0): array
     {
         d($where);
+        $debug_backtrace = debug_backtrace(1);
+        d($debug_backtrace[0]['function'] . ' ' . $debug_backtrace[0]['line'] . ' ' . $debug_backtrace[0]['file'])
+        d($debug_backtrace[1]['function'] . ' ' . $debug_backtrace[1]['line'] . ' ' . $debug_backtrace[1]['file'])
+        d($debug_backtrace[2]['function'] . ' ' . $debug_backtrace[2]['line'] . ' ' . $debug_backtrace[2]['file'])
         $set = [];
         $depth = 0;
         //convert where to array.
