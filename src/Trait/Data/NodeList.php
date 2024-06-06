c1c193bb3c9f615bab4f5ab3c532a4714c1ff940
remco van der Velde <remco.pc@outlook.com>
2024-06-06 02:13:24 +0200

    make expose_get more efficient

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -109,6 +109,10 @@ trait NodeList {
                         break;
                     }
                     d($line);
+                    $debug = debug_backtrace(1);
+                    d($debug[0]['line'] . ' ' . $debug[0]['file'] . ' ' . $debug[0]['function']);
+                    d($debug[1]['line'] . ' ' . $debug[1]['file'] . ' ' . $debug[1]['function']);
+                    d($debug[2]['line'] . ' ' . $debug[2]['file'] . ' ' . $debug[2]['function']);
                     $record = $this->index_record($line, $options);
                     $list = [];
                     if($record){
