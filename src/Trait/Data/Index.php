0f28c8190e95eeb0f6861477a27c69515c90dde2
remco van der Velde <remco.pc@outlook.com>
2024-06-08 10:41:29 +0200

    improved index_where_name

--- src/Trait/Data/Index.php
+++ src/Trait/Data/Index.php
@@ -680,7 +680,7 @@ trait Index {
             $line = $file['uuid']->current();
             $value = rtrim($line, PHP_EOL);
             $record->uuid = $value;
-            d($record);
+//            d($record);
             $record_where = $this->where($record, $options['where'], $options);
             if($record_where){
                 return $record;
