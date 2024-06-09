5299f81ebb920cfb1d5ce267748b493b63128ffc
remco van der Velde <remco.pc@outlook.com>
2024-06-10 00:04:42 +0200

    improved index_where_name

--- src/Trait/Data/Index.php
+++ src/Trait/Data/Index.php
@@ -707,6 +707,8 @@ trait Index {
                             $set_index_0 = [$set_init[0]];
                             $set_index_0 = $this->where_process($record, $set_index_0);
                             $set_index_2 = null;
+                            d($set_index_0);
+                            d($set_init);
                             //if($set_index_0[0] === true){}
                             if($set_index_0[0] === false){
                                 if($set_init[0] === false){
