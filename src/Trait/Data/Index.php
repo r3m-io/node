581ed2ef9ae395bace43aa709aa224682e2bf2ea
remco van der Velde <remco.pc@outlook.com>
2024-06-10 21:32:20 +0200

    improved index_where_name

--- src/Trait/Data/Index.php
+++ src/Trait/Data/Index.php
@@ -703,6 +703,12 @@ trait Index {
                                 d($set_init);
                                 d($where);
                                 d($deepest);
+                                $debug = debug_backtrace(1);
+                                d($debug[0]['line'] . ' ' . $debug[0]['file'] . ' ' . $debug[0]['function']);
+                                d($debug[1]['line'] . ' ' . $debug[1]['file'] . ' ' . $debug[1]['function']);
+                                d($debug[2]['line'] . ' ' . $debug[2]['file'] . ' ' . $debug[2]['function']);
+
+
                                 ddd($options);
 
                             }
