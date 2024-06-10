7e81ba5cf5a2a2e0a2336d89774e265e565eb903
remco van der Velde <remco.pc@outlook.com>
2024-06-10 18:46:40 +0200

    improved index_where_name

--- src/Trait/Data/Index.php
+++ src/Trait/Data/Index.php
@@ -777,7 +777,7 @@ trait Index {
                                             $set_index_2 = $this->where_process($record, $set_index_2);
                                             if($set_index_0[0] === true && $set_index_2[0] === true){
                                                 if(
-                                                    property_exists('uuid', $record) &&
+                                                    property_exists($record, 'uuid') &&
                                                     array_key_exists('data', $options['index']) &&
                                                     is_array($options['index']['data'])
                                                 ){
