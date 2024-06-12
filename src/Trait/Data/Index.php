f20ba5d64bd3b82828cf09fe370e8fbe86a46e5d
remco van der Velde <remco.pc@outlook.com>
2024-06-12 15:55:22 +0200

    multiple records with index

--- src/Trait/Data/Index.php
+++ src/Trait/Data/Index.php
@@ -755,8 +755,7 @@ trait Index {
                         $count_set = count($set);
                         if($count_set === 1){
                             if($operator === null && $set[0]['match'] === true){
-                                d('here2');
-                                break;
+                                return $record;
                             }
                         }
                         if($index_where){
