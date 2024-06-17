9cd8a45991644c85614304f9b57742cb5fb044b6
remco van der Velde <remco.pc@outlook.com>
2024-06-17 12:17:10 +0200

    multiple records with index

--- src/Trait/Expose.php
+++ src/Trait/Expose.php
@@ -431,6 +431,7 @@ trait Expose {
                 }
             }
         }
+        ddd($is_expose);
         ddd($nodeList);
         if($is_expose === false){
             throw new Exception(
