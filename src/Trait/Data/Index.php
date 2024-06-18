2a3db4a953683dbb4f9362c8cd4aa1aee9493ed8
remco van der Velde <remco.pc@outlook.com>
2024-06-18 19:34:42 +0200

    multiple records with index

--- src/Trait/Data/Index.php
+++ src/Trait/Data/Index.php
@@ -1223,7 +1223,6 @@ trait Index {
                                                                             $left[] = $i;
                                                                         }
                                                                         $partition = Core::array_partition($left, $options['thread'] ?? 8);
-                                                                        ddd($partition);
                                                                         $pipes = [];
                                                                         $children = [];
 // Create pipes and fork processes
@@ -1340,6 +1339,7 @@ trait Index {
                                                                                             break;
                                                                                         }
                                                                                     }
+                                                                                    d($result);
                                                                                     fwrite($sockets[0], Core::object($result, Core::OBJECT_JSON_LINE));
                                                                                     fclose($sockets[0]);
                                                                                     exit(0);
