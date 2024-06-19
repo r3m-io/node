5eaf4bb61146827f8a464cfc3e4013df4b3464f6
remco van der Velde <remco.pc@outlook.com>
2024-06-19 02:15:30 +0200

    multiple records with index

--- src/Trait/Data/Index.php
+++ src/Trait/Data/Index.php
@@ -1312,6 +1312,7 @@ trait Index {
                                                                                         $value = rtrim($line, PHP_EOL);
                                                                                         $record->uuid = $value;
                                                                                         $record_where = $this->where($record, $options['where'], $options);
+                                                                                        ddd($options);
                                                                                         if ($record_where) {
 //                                                                                            $record = $this->index_record_expose($name, $role, $record, $options);
                                                                                             $result[$i] = $record->uuid;
