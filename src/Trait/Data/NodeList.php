9ca71b7a5d2c51b8903d0ef66d01e85432c1192c
remco van der Velde <remco.pc@outlook.com>
2024-06-19 12:53:19 +0200

    multiple records with index

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -294,7 +294,7 @@ trait NodeList {
                             $list = Limit::list($list)->with([
                                 'limit' => $options['limit'],
                                 'page' => $options['page']
-                            ]);
+                            ], [], $count);
                         }
                         $record = false;
                     } else {
