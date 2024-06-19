8d87d1238b999a8074103adf6a3b8a1ad1415716
remco van der Velde <remco.pc@outlook.com>
2024-06-19 15:48:49 +0200

    multiple records with index

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -287,7 +287,7 @@ trait NodeList {
                         }
                         //one record to much, the binarysearch start
                         if($options['parallel'] === true){
-                            d($list);
+                            ddd($list);
                             d($count);
                             $partition = Core::array_partition($list, $options['thread'], false, $count-1);
                             ddd($partition);
