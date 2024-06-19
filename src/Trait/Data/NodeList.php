6e51fb4c5ff810ffb7f9c9085b8c7153286902bc
remco van der Velde <remco.pc@outlook.com>
2024-06-19 03:09:19 +0200

    multiple records with index

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -265,7 +265,6 @@ trait NodeList {
 //                $local_options['page'] = 1;
                 d($local_options);
                 $record = $this->index_list_record($class, $role, $local_options);
-                $
                 d($record);
                 $found = [];
                 while($record !== false){
