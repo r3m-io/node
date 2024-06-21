bb1c5a146b0769f5934378c7e825d365682d724b
remco van der Velde <remco.pc@outlook.com>
2024-06-21 12:05:23 +0200

    multiple records with index

--- src/Trait/Data/Index.php
+++ src/Trait/Data/Index.php
@@ -1401,7 +1401,7 @@ trait Index {
                                                                             fclose($pipe);
                                                                             $count = 0;
                                                                             if($data_url && File::exist($data_url)){
-                                                                                $data = $object->data($data_url);
+                                                                                $data = $object->data_read($data_url);
                                                                                 foreach($data as $nr => $uuid){
                                                                                     $url_ramdisk_record = $dir_ramdisk_record . $uuid . $object->config('extension.json');
                                                                                     $count++;
