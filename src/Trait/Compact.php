4eefc3a1ec578aae442cff93361dce2f363e114c
remco van der Velde <remco.pc@outlook.com>
2024-06-23 16:27:23 +0200

    multiple records with index

--- src/Trait/Compact.php
+++ src/Trait/Compact.php
@@ -2,24 +2,18 @@
 
 namespace R3m\Io\Node\Trait;
 
-use R3m\Io\App;
-use R3m\Io\Config;
-
-use R3m\Io\Module\Cli;
 use R3m\Io\Module\Controller;
-use R3m\Io\Module\Core;
-use R3m\Io\Module\Data as Storage;
-use R3m\Io\Module\File;
-use R3m\Io\Module\Parse;
 
 use Exception;
 
-use R3m\Io\Exception\FileWriteException;
 use R3m\Io\Exception\ObjectException;
-use R3m\Io\Exception\AuthorizationException;
 
 trait Compact {
 
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
     public function compact($class, $role, $options = []){
         $object = $this->object();
         $name = Controller::name($class);
