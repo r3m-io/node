95bb961b93cf9d33a7fab8296f5f7f15591c7549
remco van der Velde <remco.pc@outlook.com>
2024-06-25 16:54:38 +0200

    node object compact | expand return

--- src/Trait/Compress.php
+++ src/Trait/Compress.php
@@ -2,6 +2,7 @@
 
 namespace R3m\Io\Node\Trait;
 
+use R3m\Io\Config;
 use R3m\Io\Module\Controller;
 use R3m\Io\Module\File;
 
@@ -32,6 +33,11 @@ trait Compress {
                 'compact' => true,
                 'compress' => true
             ]);
+            if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
+                File::permission($object, [
+                    'url' => $url
+                ]);
+            }
             $duration = microtime(true) - $object->config('time.start');
             return [
                 'count' => $count,
