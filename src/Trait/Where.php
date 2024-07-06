749ce72077803b23d1d96f01157da4dc4dc07d54
remco van der Velde <remco.pc@outlook.com>
2024-07-06 08:28:05 +0200

    add ramdisk to index

--- src/Trait/Where.php
+++ src/Trait/Where.php
@@ -798,13 +798,15 @@ trait Where {
             $set = $this->where_get_set($where, $key, $deepest);
 //            d($where);
             while($record !== false){
-                ddd($set);
+                if(array_key_exists('debug', $options)) {
+                    d($set);
+                }
                 $set = $this->where_process($record, $set, $where, $key, $operator, $index_where, $options);
 //                d($where);
                 if(array_key_exists('debug', $options)){
-//                    d($where);
-//                    d($index_where);
-//                    d($set);
+                    d($where);
+                    d($index_where);
+                    ddd($set);
                 }
 
                 if(empty($set) && $deepest === 0){
