1f513c8e9373dad019172c781cbce05c99e58270
remco van der Velde <remco.pc@outlook.com>
2024-07-26 14:50:05 +0200

    make duration the new duration in create, patch & put

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -1586,6 +1586,7 @@ trait NodeList {
             return $list;
         }
         $output_filter = $options['output']['filter'];
+        d($output_filter);
         if($output_filter){
             foreach($output_filter as $output_filter_data){
                 echo $output_filter_data . PHP_EOL;
